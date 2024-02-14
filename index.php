5228bdaed205fd04503c517050ad33a7a306f5e5
Luny-TheFox <130985258+Luny-TheFox@users.noreply.github.com>
2024-02-13 21:06:56 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Decrescente</title>
+        <style>
+            .corbotao{
+                background-color:purple;
+                color:pink;
+                border-color:#af16d3;
+            }
+        </style>
+    </head>  
+    <body>
+        <form action='ex9.php' method='POST'>
+            <center>Digite um número:
+            <input type='number' name='num1'>
+            <br>
+            Digite o segundo número:
+            <input type='number' name='num2'>
+            <br>
+            Digite o terceiro número:
+            <input type='number' name='num3'>
+            <br>
+            Digite o quarto número:
+            <input type='number' name='num4'>
+            <br>
+            Digite o quinto número:
+            <input type='number' name='num5'>
+            <br>
+            Digite o sexto número:
+            <input type='number' name='num6'>
+            <br>
+            Digite o sétimo número:
+            <input type='number' name='num7'>
+            <br>
+            Digite o oitavo número:
+            <input type='number' name='num8'>
+            <br>
+            Digite o nono número:
+            <input type='number' name='num9'>
+            <br>
+            Digite o décimo número:
+            <input type='number' name='num10'>
+            <br>
+            Digite o décimo primeiro número:
+            <input type='number' name='num11'>
+            <br>
+            Digite o décimo segundo número:
+            <input type='number' name='num12'>
+            <br>
+            Digite o décimo terceiro número:
+            <input type='number' name='num13'>
+            <br>
+            Digite o décimo quarto número:
+            <input type='number' name='num14'>
+            <br>
+            Digite o décimo quinto número:
+            <input type='number' name='num15'>
+            <br>
+            Digite o décimo sexto número:
+            <input type='number' name='num16'>
+            <br>
+            Digite o décimo sétimo número:
+            <input type='number' name='num17'>
+            <br>
+            Digite o décimo oitavo número:
+            <input type='number' name='num18'>
+            <br>
+            Digite o décimo nono número:
+            <input type='number' name='num19'>
+            <br>
+            Digite o vigésimo número:
+            <input type='number' name='num20'>
+            <br>
+            <input type='submit' value='Ordenar' class='corbotao'></center>
+        </form>
+    </body>
+</html>
